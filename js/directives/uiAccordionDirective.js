b05b848968ce547e384c12d6306693a11df9c4e9
fabiodaemon <44199988+fabiodaemon@users.noreply.github.com>
2019-07-14 22:31:16 -0300

    .

--- js/directives/uiAccordionDirective.js
+++ js/directives/uiAccordionDirective.js
@@ -1,7 +1,34 @@
-angular.module("cadastroAlunos").directive("uiAccordion", function(){
+angular.module("cadastroAlunos").directive("uiAccordions", function () {
+	return {
+		controller: function ($scope, $element, $attrs) {
+			var accordions = [];
 
-    return{
-        templateUrl: "view/accordion.html"
-    };
+			this.registerAccordion = function (accordion) {
+				accordions.push(accordion);
+			};
 
+			this.closeAll = function () {
+				accordions.forEach(function (accordion) {
+					accordion.isOpened = false;
+				});
+			}
+		}
+	};
+});
+angular.module("cadastroAlunos").directive("uiAccordion", function () {
+	return {
+		templateUrl: "view/accordion.html",
+		transclude: true,
+		scope: {
+			title: "@"
+		},
+		require: "^uiAccordions",
+		link: function (scope, element, attrs, ctrl) {
+			ctrl.registerAccordion(scope);
+			scope.open = function () {
+				ctrl.closeAll();
+				scope.isOpened= true;
+			};
+		}
+	};
 });
