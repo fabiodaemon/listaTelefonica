1888c5f6d9f5b76f4076b55fd3931576c77d52b0
fabiodaemon <44199988+fabiodaemon@users.noreply.github.com>
2019-11-03 20:01:12 -0300

    fixing problem routes - angularjs >1.6

--- js/config/routeConfig.js
+++ js/config/routeConfig.js
@@ -1,4 +1,7 @@
-angular.module("cadastroAlunos").config(function ($routeProvider) {
+angular.module("cadastroAlunos").config(function ($routeProvider, $locationProvider) {
+
+	$locationProvider.hashPrefix('');
+	
 	$routeProvider.when("/contatos", {
 		templateUrl: "view/contatos.html",
 		controller: "cadastroAlunosCtrl",
@@ -20,14 +23,31 @@ angular.module("cadastroAlunos").config(function ($routeProvider) {
 			}
 		}
 	});
-	$routeProvider.when("/detalhesContato/:id", {
-		templateUrl: "view/detalhesContato.html",
-		controller: "detalhesContatoCtrl",
-		resolve: {
-			contato: function (contatosAPI, $route) {
-				return contatosAPI.getContato($route.current.params.id);
-			}
-		}
-	});
+	// $routeProvider.when("/detalhesContato/:id", {
+	// 	templateUrl: "view/detalhesContato.html",
+	// 	controller: "detalhesContatoCtrl",
+	// 	resolve: {
+	// 		contato: function (contatosAPI, $route) {
+	// 			return contatosAPI.getContato($route.current.params.id);
+	// 		}
+	// 	}
+	// $routeProvider.when("/novoContato", {
+	// 	templateUrl: "view/novoContato.html",
+	// 	controller: "novoContatoCtrl",
+	// 	resolve: {
+	// 		operadoras: function (operadorasAPI) {
+	// 			return operadorasAPI.getOperadoras();
+	// 		}
+	// 	}
+	// });
+	// $routeProvider.when("/detalhesContato/:id", {
+	// 	templateUrl: "view/detalhesContato.html",
+	// 	controller: "detalhesContatoCtrl",
+	// 	resolve: {
+	// 		contato: function (contatosAPI, $route) {
+	// 			return contatosAPI.getContato($route.current.params.id);
+	// 		}
+	// 	}
+	// });
 	$routeProvider.otherwise({redirectTo: "/contatos"});
 });
