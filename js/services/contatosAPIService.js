69f43f16fffcf206562302fd6e1947dff0c8e7c4
fabiodaemon <44199988+fabiodaemon@users.noreply.github.com>
2019-11-04 21:53:57 -0300

    problem with backend

--- js/services/contatosAPIService.js
+++ js/services/contatosAPIService.js
@@ -1,17 +1,19 @@
-//factory
+angular.module("cadastroAlunos").factory("contatosAPI", function ($http, config) {
+	var _getContatos = function () {
+		return $http.get(config.baseUrl + "/contatos");
+	};
 
-angular.module("cadastroAlunos").factory("contatosAPI", function($http, config){
-    
-    var _getContatos = function(){
-        return $http.get(config.baseUrl + "/contatos");
-    };
-    
-    var _saveContato = function(contato){
-        return $http.post(config.baseUrl + "/contatos", contato);
-    }
+	var _getContato = function (id) {
+		return $http.get(config.baseUrl + "/contatos/" + id);
+	};
 
-    return {
-        getContatos: _getContatos,
-        saveContato: _saveContato
-    };
+	var _saveContato = function (contato) {
+		return $http.post(config.baseUrl + "/contatos", contato);
+	};
+
+	return {
+		getContatos: _getContatos,
+		getContato: _getContato,
+		saveContato: _saveContato
+	};
 });
